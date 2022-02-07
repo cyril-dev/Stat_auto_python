f7466087311688a225958ea187f578bacf4abbe2
cyril-dev <cyrilkok@gmail.com>
2022-02-07 23:30:58 +0100

    init la class de calcul

--- fileCalculStat.py
+++ fileCalculStat.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 
 
 class CalculStat():
-    def __init__(self):
+    def __init__(self,path):
         super().__init__()
+        self.path = path
+        print(self.path)
         print('Calcul de stat')
+        self.data = pd.read_csv(self.path,sep=';')
