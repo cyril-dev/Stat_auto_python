825a46cff168ae48e759dc75e8b8a0cacf15dc55
cyril-dev <cyrilkok@gmail.com>
2022-02-07 20:52:03 +0100

    init projet

---
+++ fileCalculStat.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+class CalculStat():
+    def __init__(self):
+        super().__init__()
+        print('Calcul de stat')
---
+++ fileReportPdf.py
@@ -0,0 +1,5 @@
+from fpdf import FPDF
+
+class ReportPdf(FPDF):
+    def __init__(self):
+        print('Rapport PDF')
